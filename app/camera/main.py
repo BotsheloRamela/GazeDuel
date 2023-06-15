04fcf9a5678dfb14b8851b699e5bb084fac42328
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-15 10:38:43 +0200

    feat(camera): New generate function

--- app/camera/main.py
+++ app/camera/main.py
@@ -4,5 +4,13 @@ from camera import VideoCamera
 app = Flask(__name__)
 
 @app.route('/')
+
 def index():
     return render_template('page.tsx')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame +
+               b'\r\n\r\n')
