a1e34d58a78e2ff00450f2a2d5a6c6f6ba704734
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-15 10:41:19 +0200

    feat(camera): New camera video feed function

--- app/camera/main.py
+++ app/camera/main.py
@@ -14,3 +14,9 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame +
                b'\r\n\r\n')
+        
+@app.route('videp_feed')
+
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace;boundary=frame')
