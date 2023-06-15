de2114b398c76ccb55dcd4c5a686bcb1fcc0ad3b
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-15 10:33:33 +0200

    refactor(camera): Moved code from main to camera py file

--- app/camera/main.py
+++ app/camera/main.py
@@ -1,12 +1,1 @@
-
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-
-    def __del__(self):
-        self.video.release()
-
-    def get_frame(self):
-        ret, frame = self.video.read()
-        ret, jpeg = cv2.imencode('jpg', frame)
-        return jpeg.tobytes()
+from flask import Flask, render_template, Response
