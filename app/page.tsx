d7bc61cd993bc4bbbad16acefb2a0368eda65015
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-23 22:31:43 +0200

    refactor(ui): Updated UI color

--- app/page.tsx
+++ app/page.tsx
@@ -35,38 +35,38 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 h-[100vh] bg-[#282828]">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 h-[100vh] bg-[#25274d]">
       <div className='text-center'>
-        <h1 className='text-5xl font-extrabold pb-2 tracking-wider text-[#3500d3]'>GazeDuel</h1>
+        <h1 className='text-5xl font-extrabold pb-2 tracking-wider'>GazeDuel</h1>
         <h3 className='text-sm'>by</h3>
-        <h3 className='text-sm font-semibold ]'><a href="https://github.com/BotsheloRamela" target='_blank' className='underline'>Botshelo Ramela</a></h3>
+        <h3 className='text-sm font-semibold text-[#aaabb8]'><a href="https://github.com/BotsheloRamela" target='_blank' className='underline'>Botshelo Ramela</a></h3>
       </div>
 
       <div className='flex flex-row p-12 gap-64'>
         <div className='text-center'>
           <h5 className='font-medium'>Computer</h5>
-          <div className='bg-[#3500d3] flex flex-col items-center justify-evenly h-96 w-96 rounded-md mt-4 p -12'>
+          <div className='bg-[#464866] flex flex-col items-center justify-evenly h-96 w-96 rounded-md mt-4 p -12'>
             <p className='text-8xl'>🤖</p>
-            <div className='bg-[#282828] rounded-lg h-20 w-80 flex justify-evenly items-center'>
-              <div className='h-16 w-16 bg-black rounded-md text-4xl flex justify-center items-center'>🪨</div>
-              <div className='h-16 w-16 bg-black rounded-md text-4xl flex justify-center items-center'>📃</div>
-              <div className='h-16 w-16 bg-black rounded-md text-4xl flex justify-center items-center'>✂️</div>
+            <div className='bg-[#aaabb8] rounded-lg h-20 w-80 flex justify-evenly items-center'>
+              <div className='h-16 w-16 bg-[#282828] rounded-md text-4xl flex justify-center items-center'>🪨</div>
+              <div className='h-16 w-16 bg-[#282828] rounded-md text-4xl flex justify-center items-center'>📃</div>
+              <div className='h-16 w-16 bg-[#282828] rounded-md text-4xl flex justify-center items-center'>✂️</div>
             </div>
           </div>
         </div>
         <div className='text-center'>
           <h5 className='font-medium'>You</h5>
-          <div className='bg-[#3500d3] flex flex-col items-center justify-evenly h-96 w-96 rounded-md mt-4 p -12'>
+          <div className='bg-[#464866] flex flex-col items-center justify-evenly h-96 w-96 rounded-md mt-4 p -12'>
             {/* Camera   */}
             <div className='bg-violet-950 h-60 w-72 rounded-lg'>
               <div>
                 {/* <video ref={videoRef} autoPlay className='h-60'></video> */}
               </div>
             </div>
-            <div className='bg-[#282828] rounded-lg h-20 w-80 flex justify-evenly items-center'>
-            <div className='h-16 w-16 bg-black rounded-md text-4xl flex justify-center items-center'>🪨</div>
-              <div className='h-16 w-16 bg-black rounded-md text-4xl flex justify-center items-center'>📃</div>
-              <div className='h-16 w-16 bg-black rounded-md text-4xl flex justify-center items-center'>✂️</div>
+            <div className='bg-[#aaabb8] rounded-lg h-20 w-80 flex justify-evenly items-center'>
+            <div className='h-16 w-16 bg-[#282828] rounded-md text-4xl flex justify-center items-center'>🪨</div>
+              <div className='h-16 w-16 bg-[#282828] rounded-md text-4xl flex justify-center items-center'>📃</div>
+              <div className='h-16 w-16 bg-[#282828] rounded-md text-4xl flex justify-center items-center'>✂️</div>
             </div>
           </div>
         </div>
